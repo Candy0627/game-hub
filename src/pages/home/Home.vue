1f7e65b18aceb3af54c72124b18f6e6b126af8ae
zzq <1587358064@qq.com>
2020-02-23 10:06:26 +0800

    c

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <home-menu></home-menu>
+        <home-swiper></home-swiper>
+    </div>
+</template>
+
+<script>
+import HomeMenu from './components/Menu'
+import HomeSwiper from './components/Swiper'
+export default {
+    name:'Home',
+    components:{
+        HomeMenu,
+        HomeSwiper
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item in swiperList" :key="item.id">
+                <img class="swiper_img" :src="item.imgUrl" alt="">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'homeSwiper',
+    data () {
+        return {
+            swiperOption:{
+                pagination:'.swiper-pagination',
+                loop:true
+            },
+            swiperList:[{
+                id:'001',
+                imgUrl:'../../../../static/images/banner01.jpg'
+            },{
+                id:'002',
+                imgUrl:'../../../../static/images/banner02.jpg'
+            },{
+                id:'003',
+                imgUrl:'../../../../static/images/banner03.jpg'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important
+.wrapper
+    width:100%
+    overflow: hidden
+    height:0
+    padding-bottom:39.1%
+    background: #eee
+    .swiper_img
+        width: 100%
+</style>
